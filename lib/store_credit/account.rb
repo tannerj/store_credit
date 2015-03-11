46ff7ae419df8ed74fd8c685d72cc4da43e1e937
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-03-11 12:01:24 -0400

    Creates account class and tests.

---
+++ lib/store_credit/account.rb
@@ -0,0 +1,22 @@
+module StoreCredit
+  class Error < RuntimeError; end
+  class Account
+    class InsufficientFunds < StoreCredit::Error; end
+
+    attr_reader :balance
+    
+    def initialize(credit)
+      @balance = credit
+    end
+
+    def deposite(amount)
+      @balance += amount
+    end
+
+    def withdraw(amount)
+      raise InsufficientFunds if amount > @balance
+      @balance -= amount
+      amount
+    end
+  end
+end
---
+++ test/test_credit.rb
@@ -0,0 +1,31 @@
+require_relative '../lib/store_credit/account'
+require 'test/unit'
+
+class TestCredit < Test::Unit::TestCase
+  def tests_can_set_value
+    account  = StoreCredit::Account.new(100)
+    assert_equal(100, account.balance)
+  end
+
+  def tests_can_withdraw_from_credit
+    account = StoreCredit::Account.new(100)
+    assert_equal(account.withdraw(20), 20)
+  end
+
+  def tests_withdraw_reduces_balance
+    account = StoreCredit::Account.new(100)
+    account.withdraw(20)
+    assert_equal(account.balance, 80)
+  end
+
+  def tests_withdraw_throws_insufficient_funds_exception_on_overdraft
+    account = StoreCredit::Account.new(100)
+    assert_raises(StoreCredit::Account::InsufficientFunds) { account.withdraw(120)  }
+  end
+
+  def tests_deposite_correctly_updates_balance
+    account = StoreCredit::Account.new(100)
+    account.deposite(20)
+    assert_equal(account.balance, 120)
+  end
+end
