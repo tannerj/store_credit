fca73362b19d750c59cb02811c2c66c12ff58a77
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-03-12 17:05:45 -0400

    Begins Update to StoreCredit::Store to use binary search and a sorted array of items to find item matches.

--- lib/store_credit/store.rb
+++ lib/store_credit/store.rb
@@ -3,18 +3,39 @@ module StoreCredit
   
     def initialize()
       @items = []
+      @first_item_hash = nil
     end
 
     def add_item(item)
+      if @items.count == 0
+        @first_item_hash = item.hash
+      end
       @items << item
+      #sort_items
     end
 
-    def remove_item(item_properties={})
-      @items.delete_if { |item| item.position == item_properties[:position] }
+    def shift_item
+      item = @items.shift
+      #sort_items
+      item
     end
 
     def item_count
       @items.count
+    end
+
+    def items_empty?
+      @items.empty?
+    end
+
+    def get_first_item_hash
+      @first_item_hash
+    end
+
+    private
+    def sort_items
+      @sorted_items = @items
+      @sorted_items.sort! { |a,b| a.price <=> b.price }
     end
   end
 end
--- test/test_store.rb
+++ test/test_store.rb
@@ -15,13 +15,38 @@ class TestStore < Test::Unit::TestCase
     assert_equal(@store.item_count, 1)
   end
 
-  def tests_can_remove_item_from_items
+  def tests_first_item_hash_is_first_item_hash
     @store = StoreCredit::Store.new
-    @item = OpenStruct.new(position: 1, price: 10)
-    assert_equal(@store.item_count, 0)
+    @item = OpenStruct.new(position: 100, price: 2000)
     @store.add_item(@item)
-    assert_equal(@store.item_count, 1)
-    @store.remove_item(position: 1)
-    assert_equal(@store.item_count, 0)
+    @store = add_items_to_store(@store, 20)
+    assert_equal(@item.hash, @store.get_first_item_hash) 
+  end
+
+  def tests_first_item_hash_is_updated_after__items_emptied
+    @store = StoreCredit::Store.new
+    @item = OpenStruct.new(position: 100, price: 2000)
+    @store.add_item(@item)
+    @store = add_items_to_store(@store, 20)
+    #empty @store
+    @store.shift_item until @store.items_empty?
+    @new_item = OpenStruct.new(position: 1001, price: 2001)
+    @store.add_item(@new_item)
+    assert_equal(@new_item.hash, @store.get_first_item_hash)
+  end
+
+  def tests_can_shift_item_off_items_stack
+    @store = StoreCredit::Store.new
+    @store = add_items_to_store(@store, 20)
+    assert_match(/#<OpenStruct position=\d+, price=\d+>/, @store.shift_item.to_s)
+  end
+
+  def add_items_to_store(store, number_of_items)
+    number_of_items.times do |i|
+      i += 1
+      item = OpenStruct.new(position: i, price: i*10)
+      store.add_item(item)
+    end
+    store
   end
 end
