ce6d0970a484d192a8087c04e6ca15f6f15c582c
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-03-16 14:07:17 -0400

    Adds add_case and process methods to output processor to handle formatting and writing output to a file.

--- lib/store_credit/output_processor.rb
+++ lib/store_credit/output_processor.rb
@@ -1,5 +1,21 @@
 module StoreCredit
   class OutputProcessor
+    
+    def initialize()
+      @cases = []
+    end
 
+    def add_case(new_case)
+      @cases << new_case
+      new_case.id
+    end
+
+    def process
+      File.open("/Users/tanner/Desktop/output", "w") do |file|
+        @cases.each do |current_case|
+          file.puts "Case ##{current_case.id}: #{current_case.matched_items[0].position} #{current_case.matched_items[1].position}"
+        end
+      end
+    end
   end
 end
--- test/test_output_processor.rb
+++ test/test_output_processor.rb
@@ -1,8 +1,19 @@
 require 'test/unit'
+require 'ostruct'
 require_relative '../lib/store_credit/output_processor'
 
 class TestOutputProcessor < Test::Unit::TestCase
   def tests_can_instantiate_output_processor
     @output_processor = StoreCredit::OutputProcessor.new
+  end
+
+  def tests_can_add_cases_to_output_processor
+    @output_processor = StoreCredit::OutputProcessor.new
+    @items = [
+      @item_1 = OpenStruct.new(position: 1, price: 25),
+      @item_2 = OpenStruct.new(position: 2, price: 30),
+    ]
+    @case = OpenStruct.new(id: 1, matched_items: @items)
+    assert_equal(1, @output_processor.add_case(@case))
   end
 end
